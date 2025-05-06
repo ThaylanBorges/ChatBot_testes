23be01bf4439ebc6988b444d45b356d5d21a6c89
ThaylanBorges <taylan-150963@hotmail.com>
2025-05-06 16:30:00 -0300

    Adicionando o Swagger no projeto

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/router";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swaggeer.json";
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
 app.use("/api", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT;
 app.listen(port, () => {
